c010028fc62d0148433a2644adf4f1f2f7fd45ab
Isidro Navarro Oporto <99091821+IsiNavarro@users.noreply.github.com>
2023-08-13 21:01:46 +0200

    Add basic in-console-working app. No dates yet.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import './assets/style.css';
-import Task, { AllTasksList } from'./todolist.js';
+import { mainList, projectList, ListManager, Task, Project } from'./todolist.js';
 
 const Task1 = new Task('Create ToDoList', '20122023', 1, 'inbox');
-const TaskList = new AllTasksList();
-TaskList.appendTask(Task1);
-TaskList.showAllTasks();
+const Task2 = new Task('Exam', '20122023', 1, 'University');
+
+
+
+console.log(mainList);
+
+const Project1 = new Project('University');
+console.log(Project1.list);
+
+ListManager.removeTaskFromMainList(Task1, mainList);
+console.log(mainList);
+
+console.log(projectList);
+console.log(projectList[0].list)
+
--- src/todolist.js
+++ src/todolist.js
@@ -1,27 +1,69 @@
+const mainList = [];
+const projectList = [];
+
+const ListManager =  new class ListManager {
+    removeTaskFromMainList(Task) {
+        const index = mainList.indexOf(Task);
+    
+        if (index !== -1) {
+            mainList.splice(index, 1);
+        }
+    }
+    removeTaskFromProjectList(Task) {
+        const index = projectList.indexOf(Task);
+    
+        if (index !== -1) {
+            projectList.splice(index, 1);
+        }
+    }
+
+}
+
 class Task {
     constructor(_name, _date, _prio, _project) {
         this.name = _name;
         this.date = _date;
         this.prio = _prio;
         this.project = _project;
-        
         this.onGoing = true;
+        
+        mainList.push(this);
+    }
+
+    set Name(newName) {
+        this.name = newName;
+    }
+    set Date(newDate) {
+        this.date = newDate;
+    }
+    set Prio(newPrio) {
+        this.prio = newPrio;
+    }
+    set Project(newProject) {
+        this.project = newProject;
+    }
+    
+    toggleOnGoing() {
+        this.onGoing = !this.onGoing;
     }
 }
 
-class  AllTasksList {
-    constructor() {
-        this.list = [];
+class Project {
+    constructor(_name) {
+        this.name = _name;
+
+        if (projectList.length < 5) projectList.push(this);
     }
-    showAllTasks() {
-        for (let item in this.list){
-            console.log(this.list[item]);
-        }
-    }
-    appendTask(Task) {
-        this.list.push(Task);
+
+    get list() {
+        return mainList.filter(task => task.project === this.name)
     }
 }
 
-export default Task;
-export { AllTasksList };
+export {
+    mainList,
+    projectList,
+    ListManager,
+    Task,
+    Project
+};
