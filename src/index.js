098c4d87e1660133e3abd23ae827c720f4b0af04
Isidro Navarro Oporto <99091821+IsiNavarro@users.noreply.github.com>
2023-08-15 11:07:47 +0200

    Finish basic. first draft working JavaScript APP The app is working in console. Need to build the UI.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import './assets/style.css';
-import { mainList, projectList, ListManager, Task, Project } from'./todolist.js';
+import { inbox, projectList, ListManager, Task, Project } from'./todolist.js';
 
-const Task1 = new Task('Create ToDoList', '20122023', 1, 'inbox');
-const Task2 = new Task('Exam', '20122023', 1, 'University');
-
-
-
-console.log(mainList);
+const Task1 = new Task('Create ToDoList', '2023-08-16', 1, 'inbox');
+const Task2 = new Task('Exam', '2023-08-15', 1, 'University');
 
 const Project1 = new Project('University');
-console.log(Project1.list);
 
-ListManager.removeTaskFromMainList(Task1, mainList);
-console.log(mainList);
-
-console.log(projectList);
-console.log(projectList[0].list)
+const dueToday = ListManager.filterDueIn7Days(projectList[0].list);
+console.log(dueToday);
 
--- src/todolist.js
+++ src/todolist.js
@@ -1,22 +1,43 @@
-const mainList = [];
+import { addDays, sub, isWithinInterval, parseISO, isToday } from 'date-fns';
+const inbox = [];
 const projectList = [];
 
 const ListManager =  new class ListManager {
-    removeTaskFromMainList(Task) {
-        const index = mainList.indexOf(Task);
+    removeTaskFrominbox(task) {
+        const index = inbox.indexOf(task);
     
         if (index !== -1) {
-            mainList.splice(index, 1);
+            inbox.splice(index, 1);
         }
     }
-    removeTaskFromProjectList(Task) {
-        const index = projectList.indexOf(Task);
+    removeProjectFromProjectList(project) {
+        const index = projectList.indexOf(project);
     
         if (index !== -1) {
             projectList.splice(index, 1);
         }
     }
 
+    // Pass 'inbox' or 'projectList[i].list'
+    filterDueToday(list) {
+        return list.filter(task => {
+            const taskDate = parseISO(task.date);
+            return isToday(taskDate)
+        })
+    }
+
+    // Pass 'inbox' or 'projectList[i].list'
+    filterDueIn7Days(list) {
+        const today = new Date();
+        const yesterday = sub(today, {days: 1});
+        const sevenDaysFromNow = addDays(today, 7);
+
+        return list.filter(task => {
+            const taskDate = parseISO(task.date);
+            console.log("taskDate:", taskDate);
+            return isWithinInterval(taskDate, {start: yesterday, end: sevenDaysFromNow})
+        });
+    }
 }
 
 class Task {
@@ -27,22 +48,9 @@ class Task {
         this.project = _project;
         this.onGoing = true;
         
-        mainList.push(this);
+        inbox.push(this);
     }
 
-    set Name(newName) {
-        this.name = newName;
-    }
-    set Date(newDate) {
-        this.date = newDate;
-    }
-    set Prio(newPrio) {
-        this.prio = newPrio;
-    }
-    set Project(newProject) {
-        this.project = newProject;
-    }
-    
     toggleOnGoing() {
         this.onGoing = !this.onGoing;
     }
@@ -56,12 +64,12 @@ class Project {
     }
 
     get list() {
-        return mainList.filter(task => task.project === this.name)
+        return inbox.filter(task => task.project === this.name)
     }
 }
 
 export {
-    mainList,
+    inbox,
     projectList,
     ListManager,
     Task,
